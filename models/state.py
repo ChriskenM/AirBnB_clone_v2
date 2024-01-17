518a6de613dcf6c51a942beed6b4fd9ec1117469
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-17 00:46:09 -0800

    recompiled files

--- models/state.py
+++ models/state.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String 
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
