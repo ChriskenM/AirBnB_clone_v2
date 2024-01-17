37e7a35da70da8aa56632c149489d498026e8a25
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-17 04:57:06 -0800

    somethings

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Defines the DBStorage engine.
-"""
+"""Defines the DBStorage engine."""
 from os import getenv
-from models.base_model import BaseModel, Base
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -11,7 +10,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
